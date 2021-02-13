038652f4622e8b13332d250c4de9418d509b7b48
Matthias Bromisch <mbr@ashampoo.com>
2021-02-13 16:28:52 +0100

    ADD: test mul() function

--- test/calc.test.ts
+++ test/calc.test.ts
@@ -1,4 +1,4 @@
-import { add } from "../src/calc";
+import { add, mul } from "../src/calc";
 
 describe("test add function", () => {
   it("should return 15 for add(10,5)", () => {
@@ -7,5 +7,11 @@ describe("test add function", () => {
 
   it("should return 5 for add(2,3)", () => {
     expect(add(2, 3)).toBe(5);
+  });
+});
+
+describe("test mul function", () => {
+  it("should return 15 for add(10,5)", () => {
+    expect(mul(3, 5)).toBe(15);
   });
 });
