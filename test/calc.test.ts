fba4882f0b044787170acc861e321c127e00d4ea
Matthias Bromisch <mbr@ashampoo.com>
2021-02-14 10:25:18 +0100

    FIX: typo in test description

--- test/calc.test.ts
+++ test/calc.test.ts
@@ -11,7 +11,7 @@ describe("test add function", () => {
 });
 
 describe("test mul function", () => {
-  it("should return 15 for add(10,5)", () => {
+  it("should return 15 for mul(3,5)", () => {
     expect(mul(3, 5)).toBe(15);
   });
 });
