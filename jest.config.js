c464a158153f5b2b190a542a07e3accefe92482c
Naufan Rizal <naufan@buildkite.com>
2023-01-16 11:55:34 +1300

    Install and set up Buildkite Test Analytics

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  reporters: [
+    'default',
+    'buildkite-test-collector/jest/reporter'
+  ],
+  testLocationInResults: true
 };
