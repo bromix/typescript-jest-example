a8239aded1cd0afba7273cd6123d864bfff996d5
katrin <katrin.schmidpropelleraero.com.au>
2023-12-04 16:46:10 +1100

    cesium

--- jest.config.js
+++ jest.config.js
@@ -5,9 +5,6 @@ module.exports = {
     "^(\\.{1,2}/.*)\\.js$": "$1"
   },
   extensionsToTreatAsEsm: [".ts"],
-  transformIgnorePatterns: [
-      "node_modules/@cesium-engine"
-  ],
   transform: {
     // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
     // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
