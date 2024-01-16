bce46e6402db0c49473ad67d87f907d870b70cb8
katrin <katrin.schmidpropelleraero.com.au>
2024-01-16 15:58:38 +1100

    all

--- src/calc.ts
+++ src/calc.ts
@@ -1,4 +1,9 @@
+import { Color, Cartographic } from 'cesium';
+import { getImageryProvider, PropellerTerrainProvider } from '@propelleraero/cesium-loaders';
+
+
 export function add(x: number, y: number): number {
+  const c = new Color();
   return x + y;
 }
 
