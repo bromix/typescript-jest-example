be56aa26d42593cccde57860c83d951248087f07
katrin <katrin.schmidpropelleraero.com.au>
2024-01-16 17:10:22 +1100

    fail

--- test/calc.test.ts
+++ test/calc.test.ts
@@ -4,10 +4,12 @@ import { getImageryProvider, PropellerTerrainProvider } from '@propelleraero/ces
 
 describe("test add function", () => {
   it("should return 15 for add(10,5)", () => {
+    const c = new Color();
     expect(add(10, 5)).toBe(15);
   });
 
   it("should return 5 for add(2,3)", () => {
+    new PropellerTerrainProvider();
     expect(add(2, 3)).toBe(5);
   });
 });
